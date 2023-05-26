a011ff276fb1b8150d7ab1bc730ede927c44f2e4
Matthew Edmondson <matthew.edmondson@gmail.com>
2023-05-27 02:05:50 +1000

    Use fully qualified names for templated types to avoid having to provide the namespace

--- StrongStrings/Generated/StrongString.Generated.cs
+++ StrongStrings/Generated/StrongString.Generated.cs
@@ -1,36 +1,34 @@
 namespace ktsu.io.StrongStrings;
 
-
-
 public sealed record StrongString
 		: StrongStringAbstract<StrongString>;
 
 public sealed record StrongString<TValidator>
 		: StrongStringAbstract<StrongString<TValidator>, TValidator>
-		where TValidator : IValidator;
+		where TValidator : ktsu.io.StrongStrings.IValidator;
 
 public sealed record StrongString<TValidator1, TValidator2>
 	: StrongStringAbstract<StrongString<TValidator1, TValidator2>, TValidator1, TValidator2>
-	where TValidator1 : IValidator
-	where TValidator2 : IValidator;
+	where TValidator1 : ktsu.io.StrongStrings.IValidator
+	where TValidator2 : ktsu.io.StrongStrings.IValidator;
 
 public sealed record StrongString<TValidator1, TValidator2, TValidator3>
 	: StrongStringAbstract<StrongString<TValidator1, TValidator2, TValidator3>, TValidator1, TValidator2, TValidator3>
-	where TValidator1 : IValidator
-	where TValidator2 : IValidator
-	where TValidator3 : IValidator;
+	where TValidator1 : ktsu.io.StrongStrings.IValidator
+	where TValidator2 : ktsu.io.StrongStrings.IValidator
+	where TValidator3 : ktsu.io.StrongStrings.IValidator;
 
 public sealed record StrongString<TValidator1, TValidator2, TValidator3, TValidator4>
 	: StrongStringAbstract<StrongString<TValidator1, TValidator2, TValidator3, TValidator4>, TValidator1, TValidator2, TValidator3, TValidator4>
-	where TValidator1 : IValidator
-	where TValidator2 : IValidator
-	where TValidator3 : IValidator
-	where TValidator4 : IValidator;
+	where TValidator1 : ktsu.io.StrongStrings.IValidator
+	where TValidator2 : ktsu.io.StrongStrings.IValidator
+	where TValidator3 : ktsu.io.StrongStrings.IValidator
+	where TValidator4 : ktsu.io.StrongStrings.IValidator;
 
 public sealed record StrongString<TValidator1, TValidator2, TValidator3, TValidator4, TValidator5>
 	: StrongStringAbstract<StrongString<TValidator1, TValidator2, TValidator3, TValidator4, TValidator5>, TValidator1, TValidator2, TValidator3, TValidator4, TValidator5>
-	where TValidator1 : IValidator
-	where TValidator2 : IValidator
-	where TValidator3 : IValidator
-	where TValidator4 : IValidator
-	where TValidator5 : IValidator;
+	where TValidator1 : ktsu.io.StrongStrings.IValidator
+	where TValidator2 : ktsu.io.StrongStrings.IValidator
+	where TValidator3 : ktsu.io.StrongStrings.IValidator
+	where TValidator4 : ktsu.io.StrongStrings.IValidator
+	where TValidator5 : ktsu.io.StrongStrings.IValidator;
--- StrongStrings/Generated/StrongStringAbstract.Generated.cs
+++ StrongStrings/Generated/StrongStringAbstract.Generated.cs
@@ -1,51 +1,49 @@
 namespace ktsu.io.StrongStrings;
 
-
-
 public abstract record StrongStringAbstract<TDerived> : AnyStrongString<TDerived>
 	where TDerived : StrongStringAbstract<TDerived>;
 
 public abstract record StrongStringAbstract<TDerived, TValidator> : AnyStrongString<TDerived>
 	where TDerived : StrongStringAbstract<TDerived, TValidator>
-	where TValidator : IValidator
+	where TValidator : ktsu.io.StrongStrings.IValidator
 {
-	public override bool IsValid() { return base.IsValid() && Validate<TValidator, NoValidator, NoValidator, NoValidator, NoValidator>(value: this); }
+	public override bool IsValid() { return base.IsValid() && Validate<TValidator, ktsu.io.StrongStrings.NoValidator, ktsu.io.StrongStrings.NoValidator, ktsu.io.StrongStrings.NoValidator, ktsu.io.StrongStrings.NoValidator>(value: this); }
 }
 
 public abstract record StrongStringAbstract<TDerived, TValidator1, TValidator2> : AnyStrongString<TDerived>
 	where TDerived : StrongStringAbstract<TDerived, TValidator1, TValidator2>
-	where TValidator1 : IValidator
-	where TValidator2 : IValidator
+	where TValidator1 : ktsu.io.StrongStrings.IValidator
+	where TValidator2 : ktsu.io.StrongStrings.IValidator
 {
-	public override bool IsValid() { return base.IsValid() && Validate<TValidator1, TValidator2, NoValidator, NoValidator, NoValidator>(value: this); }
+	public override bool IsValid() { return base.IsValid() && Validate<TValidator1, TValidator2, ktsu.io.StrongStrings.NoValidator, ktsu.io.StrongStrings.NoValidator, ktsu.io.StrongStrings.NoValidator>(value: this); }
 }
 
 public abstract record StrongStringAbstract<TDerived, TValidator1, TValidator2, TValidator3> : AnyStrongString<TDerived>
 	where TDerived : StrongStringAbstract<TDerived, TValidator1, TValidator2, TValidator3>
-	where TValidator1 : IValidator
-	where TValidator2 : IValidator
-	where TValidator3 : IValidator
+	where TValidator1 : ktsu.io.StrongStrings.IValidator
+	where TValidator2 : ktsu.io.StrongStrings.IValidator
+	where TValidator3 : ktsu.io.StrongStrings.IValidator
 {
-	public override bool IsValid() { return base.IsValid() && Validate<TValidator1, TValidator2, TValidator3, NoValidator, NoValidator>(value: this); }
+	public override bool IsValid() { return base.IsValid() && Validate<TValidator1, TValidator2, TValidator3, ktsu.io.StrongStrings.NoValidator, ktsu.io.StrongStrings.NoValidator>(value: this); }
 }
 
 public abstract record StrongStringAbstract<TDerived, TValidator1, TValidator2, TValidator3, TValidator4> : AnyStrongString<TDerived>
 	where TDerived : StrongStringAbstract<TDerived, TValidator1, TValidator2, TValidator3, TValidator4>
-	where TValidator1 : IValidator
-	where TValidator2 : IValidator
-	where TValidator3 : IValidator
-	where TValidator4 : IValidator
+	where TValidator1 : ktsu.io.StrongStrings.IValidator
+	where TValidator2 : ktsu.io.StrongStrings.IValidator
+	where TValidator3 : ktsu.io.StrongStrings.IValidator
+	where TValidator4 : ktsu.io.StrongStrings.IValidator
 {
-	public override bool IsValid() { return base.IsValid() && Validate<TValidator1, TValidator2, TValidator3, TValidator4, NoValidator>(value: this); }
+	public override bool IsValid() { return base.IsValid() && Validate<TValidator1, TValidator2, TValidator3, TValidator4, ktsu.io.StrongStrings.NoValidator>(value: this); }
 }
 
 public abstract record StrongStringAbstract<TDerived, TValidator1, TValidator2, TValidator3, TValidator4, TValidator5> : AnyStrongString<TDerived>
 	where TDerived : StrongStringAbstract<TDerived, TValidator1, TValidator2, TValidator3, TValidator4, TValidator5>
-	where TValidator1 : IValidator
-	where TValidator2 : IValidator
-	where TValidator3 : IValidator
-	where TValidator4 : IValidator
-	where TValidator5 : IValidator
+	where TValidator1 : ktsu.io.StrongStrings.IValidator
+	where TValidator2 : ktsu.io.StrongStrings.IValidator
+	where TValidator3 : ktsu.io.StrongStrings.IValidator
+	where TValidator4 : ktsu.io.StrongStrings.IValidator
+	where TValidator5 : ktsu.io.StrongStrings.IValidator
 {
 	public override bool IsValid() { return base.IsValid() && Validate<TValidator1, TValidator2, TValidator3, TValidator4, TValidator5>(value: this); }
 }
